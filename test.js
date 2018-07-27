ba1a2fda5de3bbbf1361bd7f1e4d9a259ad2c629
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-07-27 15:19:15 -0700

    on click run test

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 /* globals assert */
+/* exported runTests */
 
 const tests = [];
 
@@ -7,6 +8,11 @@ const test = (name, testFn) => {
         name: name,
         testFn: testFn
     });
+};
+
+test.run = () => {
+    let results = runTest();
+    displayResults(results);
 };
 
 const runTest = () => {
@@ -48,9 +54,12 @@ const displayResults = (results) => {
     });
 };
 
-test('1 + 1 = 2 passes', () => assert.equal(1 + 1, 2));
 
-test('1 + 1 = 3 fails', () => assert.equal(1 + 1, 3));
+const runTests = (event) => {
+    event.preventDefault();
+    test('1 + 1 = 2 passes', () => assert.equal(1 + 1, 2)); 
+    test('1 + 1 = 3 fails', () => assert.equal(1 + 1, 3));
+        
+    test.run();
+};
 
-let results = runTest();
-displayResults(results);
