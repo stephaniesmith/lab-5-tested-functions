0063faa842e902d185a1d146005ac08c6b1bde7a
martypdx <marty@martypdx.com>
2018-07-27 10:01:54 -0700

    explored try/catch blocks

--- test.js
+++ test.js
@@ -1,3 +1,24 @@
 
+function passingTest() {
 
-bar;
+}
+
+try {
+    passingTest();
+    console.log('Test PASSED');
+}
+catch(err) {
+    console.log('Test FAILED', err.message);
+}
+
+function failingTest() {
+    throw new Error('assertion error');
+}
+
+try {
+    failingTest();
+    console.log('Test PASSED');
+}
+catch(err) {
+    console.log('Test FAILED', err.message);
+}
